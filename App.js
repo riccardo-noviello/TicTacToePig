f4005845c592ce7b738dac60fd495d2d2f3dc4e7
Riccardo <riccardo.noviello@gmail.com>
2018-10-14 17:40:45 +0100

    fix styling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View, Alert } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, Alert, TouchableHighlight } from 'react-native';
 import Image from 'react-native-remote-svg';
 
 const initialState = {
@@ -9,7 +9,8 @@ const initialState = {
     [0, 0, 0],
     [0, 0, 0]
   ],
-  currentPlayer: 1
+  currentPlayer: 1,
+  gameOver: false
 };
 
 export default class App extends Component {
@@ -19,11 +20,26 @@ export default class App extends Component {
     this.state = initialState;
   }
 
-  initialGame() {
-    this.setState(this.initialState);
+  initialiseGame() {
+    if(this.state){
+      this.setState({
+        gameState: [
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+        ],
+        currentPlayer: 1,
+        gameOver: false      
+      });
+    }
   }
 
-  renderIcon(row, col) {
+  /**
+   * Renders the players Icon for the given (row,col) coordinate.
+   * @param {*} row 
+   * @param {*} col 
+   */
+  renderPlayerIcon(row, col) {
     let value = this.state.gameState[row][col];
     switch (value) {
       case 1: return <Image source={require('./assets/pig1.png')} style={styles.image} />;
@@ -32,14 +48,25 @@ export default class App extends Component {
     }
   }
 
-  getPlayerName(player){
+  /**
+   * Returns the player name for the given player index.
+   * @param {*} player 
+   */
+  getPlayerName(player) {
     return (player == 1) ? "Peppa Pig" : "Pedro Pony";
   }
 
-  evaluateSum(sum){
+  /**
+   * Evalueates the sum is a set of Player 1 values (1+1+1) or Player 2 values (-1-1-1).
+   */
+  evaluateSum(sum) {
     return (sum == 3 || sum == -3);
   }
 
+  /**
+   * Evaluates if the game is over
+   * @param {*} arr 
+   */
   isGameFinished(arr) {
     const MAX_TILES = 3;
     let sum;
@@ -58,7 +85,7 @@ export default class App extends Component {
     // check across left to right
     sum = arr[0][0] + arr[1][1] + arr[2][2];
     result = result || this.evaluateSum(sum);
-    
+
     // check across right to left
     sum = arr[0][2] + arr[1][1] + arr[2][0];
     result = result || this.evaluateSum(sum);
@@ -66,14 +93,23 @@ export default class App extends Component {
     return result;
   }
 
+  /**
+   * Gets the next player to play
+   */
   getNextPlayer(currentPlayer) {
     return currentPlayer * (-1);
   }
 
+  /**
+   * Handles a Tile being pressed.
+   * 
+   * @param {*} row 
+   * @param {*} col 
+   */
   onTilePress(row, col) {
     // values cannot change
     let value = this.state.gameState[row][col];
-    if (value !== 0) { return; }
+    if (value !== 0 || this.state.gameOver) { return; }
 
     let currentPlayer = this.state.currentPlayer;
 
@@ -86,17 +122,25 @@ export default class App extends Component {
     let nextPlayer = this.getNextPlayer(currentPlayer);
     this.setState({ currentPlayer: nextPlayer });
 
-    if(this.isGameFinished(arr)){
-      this.showWinner(currentPlayer);
+    // finish game
+    if (this.isGameFinished(arr)) {
+      this.endGame(currentPlayer);
+      this.initialiseGame();
       return;
     }
   }
 
-  showWinner(player){
+  /**
+   * Ends the game with a given player as a winner.
+   * 
+   * @param {*} player 
+   */
+  endGame(player) {
     const playerName = this.getPlayerName(player);
-    Alert.alert('Player ' + playerName + ' wins!');
-    this.initialGame();
+    Alert.alert(playerName + ' wins!');
+    this.setState({ gameOver: true });
   }
+
 
   render() {
     return (
@@ -106,30 +150,30 @@ export default class App extends Component {
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity onPress={() => this.onTilePress(0, 0)}
             style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}>
-            {this.renderIcon(0, 0)}
+            {this.renderPlayerIcon(0, 0)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.onTilePress(0, 1)}
             style={[styles.tile, { borderTopWidth: 0 }]}>
-            {this.renderIcon(0, 1)}
+            {this.renderPlayerIcon(0, 1)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.onTilePress(0, 2)}
             style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0 }]}>
-            {this.renderIcon(0, 2)}
+            {this.renderPlayerIcon(0, 2)}
           </TouchableOpacity>
         </View>
 
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity onPress={() => this.onTilePress(1, 0)}
             style={[styles.tile, { borderLeftWidth: 0 }]}>
-            {this.renderIcon(1, 0)}
+            {this.renderPlayerIcon(1, 0)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.onTilePress(1, 1)}
             style={[styles.tile, {}]}>
-            {this.renderIcon(1, 1)}
+            {this.renderPlayerIcon(1, 1)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.onTilePress(1, 2)}
             style={[styles.tile, { borderRightWidth: 0, }]}>
-            {this.renderIcon(1, 2)}
+            {this.renderPlayerIcon(1, 2)}
           </TouchableOpacity>
         </View>
 
@@ -137,19 +181,26 @@ export default class App extends Component {
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity onPress={() => this.onTilePress(2, 0)}
             style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0 }]}>
-            {this.renderIcon(2, 0)}
+            {this.renderPlayerIcon(2, 0)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.onTilePress(2, 1)}
             style={[styles.tile, { borderBottomWidth: 0 }]}>
-            {this.renderIcon(2, 1)}
+            {this.renderPlayerIcon(2, 1)}
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.onTilePress(2, 2)}
             style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
-            {this.renderIcon(2, 2)}
+            {this.renderPlayerIcon(2, 2)}
           </TouchableOpacity>
         </View>
 
-
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight onPress={this.initialiseGame}
+            underlayColor="white">
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>New Game</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
 
       </View>
     );
@@ -164,9 +215,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
-    margin: 10,
+    marginBottom: 50,
   },
   tile: {
     borderWidth: 5,
@@ -177,4 +228,17 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginLeft: 10
   },
+  buttonContainer: {
+    paddingTop: 50
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  }
 });
